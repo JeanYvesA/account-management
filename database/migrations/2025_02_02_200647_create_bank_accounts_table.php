0a027a8dbe4e688b7a1c56d6bb1f7d418edb7e61
Jean-Yves AGBESSI <jeagbessi@gmail.com>
2025-02-02 22:34:15 +0100

    Initialisation

---
+++ database/migrations/2025_02_02_200647_create_bank_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bank_accounts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('number');
+            $table->string('name');
+            $table->string('firstname');
+            $table->decimal('balance', 10, 2)->default(0);
+            $table->string('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('statut')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bank_accounts');
+    }
+};
